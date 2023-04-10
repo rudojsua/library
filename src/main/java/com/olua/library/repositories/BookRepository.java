1bd1b22a8cec72ee0e75750dca15bc1b0514885e
Ol Ua <rudojsua@gmail.com>
2023-04-10 23:28:59 +0200

    com 2

--- src/main/java/com/olua/library/repositories/BookRepository.java
+++ src/main/java/com/olua/library/repositories/BookRepository.java
@@ -1,13 +1,12 @@
 package com.olua.library.repositories;
 
 import com.olua.library.model.Book;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
-import org.springframework.data.jdbc.repository.query.Query;
+ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-@EnableJdbcRepositories
+@Repository
 public interface BookRepository extends CrudRepository <Book, Long> {
 
     @Query("SELECT * FROM book WHERE id= :id")
